5ce6804b90ebd328db30d6db5373046b6bb5c5e1
rasel01 <raseluiu.ac.bd@gmail.com>
2017-07-31 00:29:34 +0600

    configration done

---
+++ ExampleApiApp/Configuration/ApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ExampleApiApp.Configuration
+{
+    public static class ApiConfig
+    {
+        public static void Register(HttpConfiguration configuration)
+        {
+            configuration.MapHttpAttributeRoutes();
+
+            configuration.Routes.MapHttpRoute(
+                name: "HomeApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+        }
+    }
+}
